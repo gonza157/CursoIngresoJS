301a6d50c4cc2ce13cba2cb8b134961df4559828
gonza157 <36174607+gonza157@users.noreply.github.com>
2018-02-06 16:06:52 -0300

    ejercicio 2
    solucionando lo que guarde mal

--- jsEntradaSalida-2.js
+++ jsEntradaSalida-2.js
@@ -2,24 +2,6 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	// esto es un comentario
-
-	// alert("Esto funciona de maravilla");
-	var nombre;
-	/*
-	es en bloque
-	alert("nombre");
-	alert(nombre);
-	nombre="gonza";
-	alert(nombre);
-	*/
-	nombre=prompt("ingrese su nombre","natalia natalia");
-/*
-	alert("su nombre es :);
-	alert(nombre)
-*/
-alert("nombre es: "+ nombre);
-//prompt("Please enter your name", "Harry Potter");
 	
 }
 
