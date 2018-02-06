98ea922a02bd12591190f2d43a8dc659553810d5
gonza157 <36174607+gonza157@users.noreply.github.com>
2018-02-05 21:04:04 -0300

    ejercicio 1 y 2
    arreglo de primer ejercicio.

---
+++ jsEntradaSalida-1.js
@@ -0,0 +1,10 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+function Mostar()
+{
+
+alert("esto funciona de maravilla");
+
+
+    //prompt("Please enter your name", "Harry Potter");
+}
+
