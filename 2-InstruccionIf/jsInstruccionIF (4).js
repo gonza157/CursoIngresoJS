bbde1c2f8ffe1b71577d8e3d8bbc3ae98bd2e124
gonza157 <36174607+gonza157@users.noreply.github.com>
2018-02-06 21:45:08 -0300

    ejercicio 3,4,5
    resueltos if

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,13 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
+edad= document.getElementById('edad').value;
 
+if(edad>12 && edad<17)
+{
+	alert(" Es adolecente ");
+}
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,13 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
+edad= document.getElementById('edad').value;
+
+if( ! (edad>12 && edad<17))
+{
+	alert(" no es adolecente ");
+}
 
 
 }//FIN DE LA FUNCIÓN
