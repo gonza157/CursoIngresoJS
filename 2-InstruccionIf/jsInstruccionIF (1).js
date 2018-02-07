2819314a488292e37fef9f5cb0439b9427991cd9
gonza157 <36174607+gonza157@users.noreply.github.com>
2018-02-06 21:42:59 -0300

    ejercicios 1
    resuelto

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -2,6 +2,13 @@ function Mostrar()
 {
 //tomo la edad  
 
+	var edad;
+	edad= document.getElementById('edad').value;
 
+	if(edad>15)
+		{
+			alert( " es mayor de edad ");
 
+		}		
+	
 }//FIN DE LA FUNCIÓN
