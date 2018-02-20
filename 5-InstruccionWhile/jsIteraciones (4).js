df158c338f17c833154c33bd27e8c29662e640cc
gonza157 <36174607+gonza157@users.noreply.github.com>
2018-02-19 22:12:08 -0300

    ej 4
    hecho correctamente

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,31 @@
 function Mostrar()
 {
+	var numero1;
+	var numero=prompt("ingresar un muero entre 0 y 10");
+	numero1= parseInt(numero);
 
+	while(numero1<0 || numero1 >10)
+	{
+		numero=prompt("ingresar un muero entre 0 y 10");
+		numero1= parseInt(numero);
+	}
+
+	document.getElementById('Numero').value= numero1;
+}
+	
+
+
+/*var numero1;
 	var numero = prompt("ingrese un número entre 0 y 10.");
+	numero1= parseInt(numero);
+
+	while (numero1<0 || numero1>10)
+	{
+		numero = prompt("ingrese un número entre 0 y 10.");	
+
+	}
+
+	document.getElementById('Numero').Value = numero1;
+	*/
 
 
-}//FIN DE LA FUNCIÓN
