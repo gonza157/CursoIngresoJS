b100bf1b841bfc3f2dc18020e49f99caf1c0ccfe
gonza157 <36174607+gonza157@users.noreply.github.com>
2018-02-20 01:37:03 -0300

    ej 6
    falta terminar.

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,7 +3,18 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	
+	prompt("ingrese un numero");
+	
+	
+	while(contador<4)
+	{
+		contador= contador +1;
+		acumulador=prompt("ingresar siguiente numero");		
 
+	}
+
+acumulador= acumulador + acumulador;
 
 
 document.getElementById('suma').value=acumulador;
