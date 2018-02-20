2db343dc18f2d6623cb2edb552d87d5e5d477e50
gonza157 <36174607+gonza157@users.noreply.github.com>
2018-02-19 21:31:00 -0300

    ej 3 y 7
    ej 3 bien ej 7 copmensado para terminar despues

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -3,5 +3,15 @@ function Mostrar()
 
 var clave = prompt("ingrese el número clave.");
 
+while(clave != "utn750")
+{
+	alert("error clave incorrecta");
+	var clave = prompt("ingrese el número clave.");
+
+}
+
+alert("vien venido");
+
+
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,7 +4,16 @@ function Mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+	var numero;
+	var numero1;
 
+while(respues="no")
+{
+	numero=prompt("ingresar nota");
+	numero1= parseint(numero)
+	acumulador= acumulador + numero1;
+	
+}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
