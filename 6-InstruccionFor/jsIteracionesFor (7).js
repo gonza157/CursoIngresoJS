76586a659978f4e5a19bfaef9abe426d68b96f84
gonza157 <36174607+gonza157@users.noreply.github.com>
2018-02-20 22:12:11 -0300

    ej 7
    resulto por el profe ver la logica y pensar

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,7 +1,25 @@
 function Mostrar()
 {
 
+var contadordiv=0;
+var numero=prompt("ingrese numer");
+numero= parseInt(numero);
 
 
+for(anterior=2;anterior<numero;anterior++)
+{
+	if(numero%anterior==0)
+	{
+		document.write("<br> "+anterior);
+	}
+	if(anterior>(numero/2))
+	{
+		break;
+	}
+}
 
+if(contadordiv==0)
+{
+	document.write( "  es primo ");
+}
 }//FIN DE LA FUNCIÓN
