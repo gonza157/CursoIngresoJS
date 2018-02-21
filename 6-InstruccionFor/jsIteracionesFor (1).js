388967d1e9ae56c787a9364e6be51b1818884b35
gonza157 <36174607+gonza157@users.noreply.github.com>
2018-02-20 21:38:04 -0300

    ej 1
    para entender el for

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,20 @@
 function Mostrar()
 {
+var contador=0;
 
+for(;;)
+{
+	contador++
+	document.write("<br>"+contador);
+	if(contador==10)
+	{
+		document.write("<br>break" +contador);
+		break;
+	}
+	if(contador==5)
+	{
+		continue;
+	}
+	document.write("<br>continue" +contador);
+}
 }
