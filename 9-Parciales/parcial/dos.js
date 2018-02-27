15b350dce3c2d8205eaca517c276ae74a807aa70
gonza157 <36174607+gonza157@users.noreply.github.com>
2018-02-27 01:20:24 -0300

    Add files via upload

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,16 @@
 function Mostrar()
 {
+	var importe;
+  	var importemasiva;
+  	var importeprimario
+	var importecondescuento;
+
+  importeprimario=prompt("Introducir precio");
+  importecondescuento=prompt("Introducir precio");
+  importe=parseInt(importeprimario);
+
+  importemasiva=importe + importe * 0.21;
+
+  document.getElementById('importeFinal').value=importemasiva
   
 }
