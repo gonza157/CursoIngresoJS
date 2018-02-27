ecc3e0f4ff65c51b5e59d09e479f6b59b0497b74
gonza157 <36174607+gonza157@users.noreply.github.com>
2018-02-27 02:28:17 -0300

    ejercicio

--- 9-Parciales/parcial2/6-iteraciones.js
+++ 9-Parciales/parcial2/6-iteraciones.js
@@ -1,7 +1,34 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 6-iteraciones");
+	var numero;
+	var mayor=0;
+	var menor=0;
+	var contador=0;
+
+		numero=prompt("Ingresar numero");
+		numero= parseInt(numero);
+	while(numero>0 && contador<7)
+	{
+		
+		contador++
+		while()
+		{	
+			numero=prompt("Ingresar numero");		{	
+		
+		}
+			if(numero>mayor)
+				{
+					mayor=numero;
+				}
+			if(numero<menor)
+				{
+					menor=numero;
+				}
+		}	
 	
+	}
+		alert("El numero mayor es "+mayor);
+		alert("El numero menor es "+menor);
 }
 
