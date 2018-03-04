073bec06eb138fa2d539caacee2a208c25ef09b4
gonza157 <36174607+gonza157@users.noreply.github.com>
2018-03-04 20:25:16 -0300

    ejercicios 1,2,3,4

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,6 +1,41 @@
 function Mostrar()
 {
-var numerouno;
+	var numero1;
+	var numero2;
+	var resta;
+	var multiplicacion;
+	var suma;
+
+	numero1=prompt("ingresar primer valor");
+	numero2=prompt("ingresar primer valor");
+	numero1=parseInt(numero1);
+	numero2=parseInt(numero2);
+
+	if(numero1>numero2)
+	{
+		resta=numero1-numero2;
+		alert("la resta es "+resta);
+	}else
+		{
+			if (numero1==numero2) 
+			{
+				multiplicacion=numero1*numero2;
+				alert("La multiplicacion da "+multiplicacion);
+			}else
+				{
+					suma=numero1+numero2;
+					alert("La suma da "+suma);
+				}
+		}
+
+
+
+
+
+
+
+
+	/*var numerouno;
 	var numerodos;
 	var numerotres;
 	var numerouno1;
@@ -52,5 +87,5 @@ var numerouno;
 	}
 
 	alert("El numero mayor es "+ Mayor);
-	alert("El numero menor es "+ Menor);
+	alert("El numero menor es "+ Menor);*/
 }
--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,17 @@
 function Mostrar()
 {
+	var largo;
+	var ancho;
+	var suma;
+	var perimetroalambre;
+
+	largo=document.getElementById('alrgo').value;
+	ancho=document.getElementById('ancho').value;
+	largo=parseInt(largo);
+	ancho=parseInt(ancho);
+
+	suma=largo*2 + ancho*2;
+	perimetroalambre= suma*3;
+	alert("Se necesitan "+perimetroalambre+ " de alambre");
 
 }
