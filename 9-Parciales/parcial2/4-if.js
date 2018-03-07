cd7d37fdd5916376b694f0e4b0eeb772bcf9f603
gonza157 <36174607+gonza157@users.noreply.github.com>
2018-03-07 17:03:10 -0300

    ej 4
    practica

--- 9-Parciales/parcial2/4-if.js
+++ 9-Parciales/parcial2/4-if.js
@@ -1,7 +1,40 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 4-if");
+	var numero;
+	var letra;
+	var numeromax=0;
+	var letramax;
+	var contador=0;
+
+	while(contador<3)
+	{
+		numero=prompt("ingrese numero");
+		letra=prompt("ingrese letra");
+		numero=parseInt(numero);
+
+		while(numero<0 || isNaN(numero) )
+		{
+			numero=prompt("ingrese numero");
+			numero=parseInt(numero);
+
+		}
+
+		while( !isNaN(letra))
+			{
+				letra=prompt("ingrese letra");
+			}
+
+			if(numero> numeromax)
+				{
+					numeromax=numero;
+					letramax=letra;
+				}
+		
+		contador++;
+	}
+	alert("El numero maximo es "+numeromax+ "La letra es"+ letramax);
+
 	
 }
 
